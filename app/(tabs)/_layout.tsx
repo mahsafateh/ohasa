a58c4e24f466cfa4ceef0d162dd545b4160bb99d
mahsafateh <mahsafaateh@gmail.com>
2025-07-23 14:57:37 +0200

    a minor logic issue of auth solved

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -2,6 +2,6 @@ import { Redirect, Slot } from "expo-router";
 
 export default function _Layout() {
   const isAuthenticated = true;
-  if (isAuthenticated) return <Redirect href="/sign-in" />;
+  if (!isAuthenticated) return <Redirect href="/sign-in" />;
   return <Slot />;
 }
